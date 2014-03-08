d9edc82adf2ba822043944334d548a6295722b40
Mike Davies <isofarro2@gmail.com>
2014-03-08 20:21:36 +0000

    Add beanstalk install, configuration, and service

--- puppet/base.pp
+++ puppet/base.pp
@@ -28,3 +28,31 @@ package { 'php5-cli':
 	ensure  => installed,
 	require => Exec['aptupdate'],
 }
+
+
+#
+# Beanstalk
+#
+package { 'beanstalkd':
+	ensure  => installed,
+	require => Exec['aptupdate'],
+}
+
+file { '/etc/default/beanstalkd':
+	ensure  => present,
+	source  => 'puppet:///modules/beanstalk/beanstalkd.conf',
+	owner   => 'root',
+	group   => 'root',
+	mode    => '644',
+	notify  => Service['beanstalkd'],
+	require => Package['beanstalkd'],
+}
+
+service { 'beanstalkd':
+	ensure     => running,
+	enable     => true,
+	hasrestart => true,
+	hasstatus  => true,
+	require    => Package['beanstalkd'],
+}
+
