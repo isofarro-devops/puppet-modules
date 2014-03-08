f0eb5c59acecbda191ea8176dd133b2cecc2a680
Mike Davies <isofarro2@gmail.com>
2014-03-08 20:19:31 +0000

    Install baseline packages

--- puppet/base.pp
+++ puppet/base.pp
@@ -1,1 +1,21 @@
-include baseconfig
+# TODO: Separate into modules.
+
+
+#
+# Base install
+#
+exec { 'aptupdate':
+	command => '/usr/bin/apt-get update',
+	user    => 'root',
+}
+
+package { 'core-packages':
+	name => [
+		'wget',
+		'vim',
+		'git-core',
+		'htop',
+	],
+	ensure  => installed,
+	require => Exec['aptupdate'],
+}
