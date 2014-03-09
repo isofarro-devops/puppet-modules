6e1bab9432b39f9d7d5514e3faf8a7e1b02e1f0f
Mike Davies <isofarro2@gmail.com>
2014-03-09 18:02:01 +0000

    Add php5-fpm resources

--- puppet/modules/php/manifests/init.pp
+++ puppet/modules/php/manifests/init.pp
@@ -5,10 +5,45 @@ class php {
 	package { 'php5-baseline':
 		name => [
 			'php5-cli',
+			'php5-fpm',
 			'php-pear',
 		],
 		ensure  => installed,
 		require => Package['core-packages'],
+	}
+
+	file { 'php5-fpm-ini':
+		path    => '/etc/php5/fpm/php.ini',
+		ensure  => present,
+		source  => 'puppet:///modules/php/fpm-php.ini',
+		owner   => 'root',
+		group   => 'root',
+		mode    => '644',
+		require => Package['php5-baseline'],
+		notify  => Service['php5-fpm'],
+	}
+
+	file { 'php5-fpm-www-conf':
+		path    => '/etc/php5/fpm/pool.d/www.conf',
+		ensure  => present,
+		source  => 'puppet:///modules/php/fpm-pool-www.conf',
+		owner   => 'root',
+		group   => 'root',
+		mode    => '644',
+		require => Package['php5-baseline'],
+		notify  => Service['php5-fpm'],
+	}
+
+	service { 'php5-fpm':
+		ensure     => running,
+		enable     => true,
+		hasrestart => true,
+		hasstatus  => true,
+		require    => [
+			Package['php5-baseline'],
+			File['php5-fpm-ini'],
+			File['php5-fpm-www-conf'],
+		],
 	}
 
 	exec { 'update-pear':
