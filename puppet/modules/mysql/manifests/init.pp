c3c2ea081f7fbf2b2fa274a88c0a328d21feef1c
Mike Davies <isofarro2@gmail.com>
2014-03-15 07:34:23 +0000

    Set root password on mysql. and copy in a mysql config.

--- puppet/modules/mysql/manifests/init.pp
+++ puppet/modules/mysql/manifests/init.pp
@@ -11,13 +11,39 @@ class mysql {
 		require => Package['core-packages'],
 	}
 
+
+	file { '/var/lib/mysql/my.cnf':
+		owner   => 'mysql',
+		group   => 'mysql',
+		source  => 'puppet:///modules/mysql/my.cnf',
+		notify  => Service['mysql'],
+		require => Package['mysql']
+	}
+
+
+	file { '/etc/my.cnf':
+		require => File['/var/lib/mysql/my.cnf'],
+		ensure  => '/var/lib/mysql/my.cnf'
+	}
+
+
+	exec { 'set-mysql-password':
+		unless => 'mysqladmin -uroot -prootpwd status',
+		command => 'mysqladmin -uroot password rootpwd',
+		path   => ['/bin', '/usr/bin'],
+		require => [
+			Package['mysql'],
+		]
+	}
+
+
 	service { 'mysql':
 		ensure     => running,
 		enable     => true,
 		hasrestart => true,
 		hasstatus  => true,
 		require    => [
-			Package['mysql']
+			Package['mysql'],
 		],
 
 	}
